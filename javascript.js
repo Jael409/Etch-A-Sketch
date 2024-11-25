63b68467b59ec8e392290c01a79d0bea8a858df9
Jael <jaelbuffa@gmail.com>
2024-11-25 15:50:09 +0100

    Add slider and input for value of resolution

--- javascript.js
+++ javascript.js
@@ -1,22 +1,28 @@
 let cuadricula = 600;
 
-let rows = 16;
-let columns = 16;
+let squaresPerSide = 16;
 
 let sketchArea = document.querySelector("#container");
-sketchArea.style.width = `${cuadricula}px`;
-sketchArea.style.height = `${cuadricula}px`;
+let slideContainer = document.querySelector("#sliderContainer");
+let slider = document.querySelector("#slider");
+let sliderValue = document.querySelector("#valueSlider");
+
+sliderValue.textContent = `${slider.value} x ${slider.value} (Resolution)`;
+
+sketchArea.style.width = sketchArea.style.height = `${cuadricula}px`;
 
 function changeBackgroundColor () {
     this.style.backgroundColor = "black";
 }
 
-function createrGridCells() {
-    for (i = 0; i < (rows * columns); i++) {
+function createrGridCells(squaresPerSide) {
+    const numOfSquares = (squaresPerSide * squaresPerSide);
+    const widthOrHeight = (`${(cuadricula / squaresPerSide) - 2}px`);
+
+    for (i = 0; i < numOfSquares; i++) {
         const gridCell = document.createElement("div");
 
-        gridCell.style.width = `${(cuadricula / columns) - 2}px`;
-        gridCell.style.height = `${(cuadricula / rows) - 2}px`;
+        gridCell.style.width = gridCell.style.height = `${(cuadricula / squaresPerSide) - 2}px`;
         gridCell.classList.add = ("cell");
 
         sketchArea.appendChild(gridCell); 
@@ -25,6 +31,19 @@ function createrGridCells() {
     }
 }
 
-createrGridCells();
+function removeGridCells () {
+    while (sketchArea.firstChild) {
+        sketchArea.removeChild(sketchArea.firstChild);
+    }
+}
+
+slider.oninput = function () {
+    let txt = `${this.value} x ${this.value}(Resolution)`;
+    sliderValue.textContent = txt;
+    removeGridCells();
+    createrGridCells(this.value);
+}
+
+createrGridCells(16);
 
 
